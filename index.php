d18bcc8012c1701a3f6a466e2de1a760064bb7de
aires-caala <airescaala@gmail.com>
2023-10-30 13:43:53 +0000

    Deleted files about the virtual host

--- index.php
+++ index.php
@@ -1,16 +1,20 @@
 <?php 
 
 require_once("vendor/autoload.php");
+use Hcode\Page;
 
 $config = ['settings' => ['displayErrorDetails' => true]];
 $app = new \Slim\App($config);
 
 $app->get('/', function($request, $reponse) {
     
-	//echo "OK, it's correct now!";
+	/*echo "OK, it's correct now!";
 	$sql = new Hcode\DB\Sql();
 	$results = $sql->select("SELECT * FROM tb_users");
 	echo json_encode($results);
+	*/
+	$page = new Page();
+
 
 });
 
