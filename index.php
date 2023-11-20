f32774129ec1b8e3f4b3aa36ea30416e393114c7
aires-caala <airescaala@gmail.com>
2023-11-20 21:33:44 +0000

    tpl site

--- index.php
+++ index.php
@@ -2,22 +2,15 @@
 
 require_once("vendor/autoload.php");
 
+use \Slim\App;
+use \Hcode\Page;
+
 $config = ['settings' => ['displayErrorDetails' => true]];
-$app = new \Slim\App($config);
+$app = new App($config);
 
 $app->get('/', function($request, $reponse) {
-    
-	/*echo "OK, it's correct now!";
-	$sql = new Hcode\DB\Sql();
-	$results = $sql->select("SELECT * FROM tb_users");
-	echo json_encode($results);
-	*/
-	//$page = new Page();
-	//$page->setTpl("index");
-
-	$page = new Hcode\Page();
+	$page = new Page();
 	$page->setTpl("index");
-
 });
 
 $app->run();
