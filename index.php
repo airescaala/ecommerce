6360b57041b3c5a4e3fcc80a82131d25eb7a748b
aires-caala <airescaala@gmail.com>
2023-11-21 18:35:21 +0000

    confugurar o template admin

--- index.php
+++ index.php
@@ -4,12 +4,18 @@ require_once("vendor/autoload.php");
 
 use \Slim\App;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $config = ['settings' => ['displayErrorDetails' => true]];
 $app = new App($config);
 
 $app->get('/', function($request, $reponse) {
 	$page = new Page();
+	$page->setTpl("index");
+});
+
+$app->get('/admin', function($request, $reponse) {
+	$page = new PageAdmin();
 	$page->setTpl("index");
 });
 
